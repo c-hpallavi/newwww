25282642d47ca032dd485b42588f50024a453775
c-hpallavi <139103495+c-hpallavi@users.noreply.github.com>
2024-03-05 20:32:17 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,25 @@
+package com.kodnest.strings;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+      String s1=new String("RAMA");
+      String s2=new String("RAMA");
+      if(s1==s2)
+      {
+    	  System.out.println("references are equal");
+      }
+      else
+      {
+    	  System.out.println("references are unequal");
+      }
+      if(s1.equals(s2))
+      {
+    	  System.out.println("strings are equal");
+      }
+      else {
+    	  System.out.println("strings are unequal");
+      }
+	}
+
+}
